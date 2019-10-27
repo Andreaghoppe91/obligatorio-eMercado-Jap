7c98da8f5a505a1619816b124fca587cbe96e8ef
Andreaghoppe91 <54375344+Andreaghoppe91@users.noreply.github.com>
2019-10-27 16:34:51 -0300

    cart

--- ecommerce-master obligatorio/js/cart.js
+++ ecommerce-master obligatorio/js/cart.js
@@ -1,8 +1,8 @@
 let productUnitCost = 0;
 let productCurrency = "";
 let MONEY_SYMBOL = "$";
-let PERCENTAGE_SYMBOL = '%';
 let subtotal = 0;
+let PERCENTAGE_SYMBOL = '%';
 let cantidadSeleccionada = "";
 let shippingPercentage = 0.15;
 let total = 0;
@@ -11,19 +11,8 @@ const CREDIT_CARD_PAYMENT = "Tarjeta de crédito";
 const BANKING_PAYMENT = "Transferencia bancaria";
 let ERROR_MSG = "Ha habido un error :(, verifica qué pasó.";
 
+
 //Función que se utiliza para actualizar los costos de publicación
-
-function updateTotalCosts(){
-    let precioProdSelect = document.getElementById("precioproduct");
-    let comissionEnvio = document.getElementById("comisionEnvio");
-    let totalCost = document.getElementById("costototal");
-
-    precioProdSelect.innerHTML = MONEY_SYMBOL + productUnitCost;
-    comissionEnvio.innerHTML = Math.round((comissionPercentage * 100)) + PERCENTAGE_SYMBOL;
-    totalCost = MONEY_SYMBOL + (Math.round(productUnitCost * comissionPercentage * 100) / 100);
-    
-    document.getElementById("precioproduct").innerHTML = subTotal;
-}
 
 function updateSubtotal(precioUnitario){
     let cantidad = document.getElementById("productCantInput").value;
@@ -43,7 +32,7 @@ function showArticles(array){
         <td>`+ articles.name +`</td>
         <td>`+ articles.currency +`</td>
         <td>`+ articles.unitCost +`</td>
-        <td><input type="number" name="cantidad" id="productCantInput" value="`+articles.count +`" min="1"></td>
+        <td><input type="number" name="productInput" id="productCantInput" value="`+articles.count +`" min="1"></td>
         <td id="subtotal"></td>
         </tr>
         `
@@ -52,14 +41,16 @@ function showArticles(array){
         
     precioUnitario = articles.unitCost;
     updateSubtotal(precioUnitario);
-    document.getElementById("productCantInput").addEventListener("change", function(){
-        updateSubtotal(precioUnitario);
-    });
     }
 }
-
 function updateTotalCosts(){
+    let precioProdSelect = document.getElementById("precioproduct");
+    let comissionEnvio = document.getElementById("comisionEnvio");
+    let total = document.getElementById("costototal");
 
+    precioProdSelect.innerHTML = MONEY_SYMBOL + cantidadSeleccionada * 100;
+    comissionEnvio.innerHTML = Math.round((shippingPercentage * 100)) + PERCENTAGE_SYMBOL;
+    total.innerHTML = MONEY_SYMBOL + (Math.round(cantidadSeleccionada * shippingPercentage * 100)+ (cantidadSeleccionada * 100));
 }
 
 function showPaymentTypeNotSelected(){
@@ -76,6 +67,27 @@ document.addEventListener("DOMContentLoaded", function(e){
         {   
             carrito = resultObj.data;
             showArticles(carrito.articles);
+            updateTotalCosts();
         }
+        
+    document.getElementById("productCantInput").addEventListener("change", function(){
+        cantidadSeleccionada = this.value;
+        updateTotalCosts();
+    });
+
+    document.getElementById("radioexp").addEventListener("change", function(){
+        shippingPercentage = 0.07;
+        updateTotalCosts();
+    });
+    
+    document.getElementById("radioprem").addEventListener("change", function(){
+        shippingPercentage = 0.15;
+        updateTotalCosts();
+    });
+    
+    document.getElementById("radiostand").addEventListener("change", function(){
+        shippingPercentage = 0.05;
+        updateTotalCosts();
+    });
 });
 });
